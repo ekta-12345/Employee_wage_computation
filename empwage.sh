1406de4b797a5092349c4dc8e4dcb6a67ed4297d
ekta12345 <ektakumbhare3@gmail.com>
2021-03-18 23:21:09 +0530

    Added: code for employee present or absent

--- empwage.sh
+++ empwage.sh
@@ -1,1 +1,9 @@
 echo "Welcome to Employee Wage Computation Program "
+EmpPresent=1
+random=$(($RANDOM%2))
+if [ $random -eq $EmpPresent ]
+then
+   echo "Employee is Present"
+else
+   echo "Employee is Absent"
+fi
