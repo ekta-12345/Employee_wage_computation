5b8e33df62b1b294b70ed2962c038901faac630c
ekta12345 <ektakumbhare3@gmail.com>
2021-03-19 00:02:40 +0530

    Added:calculating wages for a month

--- empwage.sh
+++ empwage.sh
@@ -2,6 +2,11 @@ echo "Welcome to Employee Wage Computation Program "
 isfulltime=1
 ispartime=2
 Wagepr_hr=20
+totalsalary=0
+nof_wdays=20
+
+for (( day=1 ; day<=$nof_wdays ; day++ ))
+do
 random=$(($RANDOM%3))
 
 case $random in 
@@ -15,5 +20,7 @@ case $random in
 	  emp_hrs=0
 	  ;;
 esac
-Dailywage=$(($emp_hrs*$Wagepr_hr))
-echo "Daily wage of Employee is:" $Dailywage
+done
+Salary=$(($emp_hrs*$Wagepr_hr))
+totalsalary=$(($totalsalary+$Salary))
+echo "Total salary of employee is:" $totalsalary
