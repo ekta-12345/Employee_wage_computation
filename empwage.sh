1269e7db01d60d4b4d87c6dce2c4c3675206ede7
ekta12345 <ektakumbhare3@gmail.com>
2021-03-18 23:39:59 +0530

    Added:code using switch case

--- empwage.sh
+++ empwage.sh
@@ -2,17 +2,18 @@ echo "Welcome to Employee Wage Computation Program "
 isfulltime=1
 ispartime=2
 Wagepr_hr=20
-Partime_hr=4
-Fullday_hr=8
 random=$(($RANDOM%3))
-if [ $random -eq $isfulltime ]
-then
-    Dailywage=$(( $Wagepr_hr * $Fullday_hr ))
-    echo "Daily wage of Fulltime Employee is:" $Dailywage
-elif [ $random -eq $ispartime ]
-then
-    Dailywage=$(( $Wagepr_hr * $Partime_hr ))
-    echo "Daily wage of Parttime Employee is:" $Dailywage
-else
-    echo "Employee is Absent"
-fi
+
+case $random in 
+     $isfulltime)
+   	  emp_hrs=8
+          ;;
+     $ispartime)
+	  emp_hrs=4
+	  ;;
+     *)
+	  emp_hrs=0
+	  ;;
+esac
+Dailywage=$(($emp_hrs*$Wagepr_hr))
+echo "Daily wage of Employee is:" $Dailywage
